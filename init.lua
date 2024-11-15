0d58b86b85e60e254bc06e0debc96c9453bf4053
SeqSEE <SeqSEE@users.noreply.github.com>
2024-11-14 19:44:52 -0500

    update blood

--- init.lua
+++ init.lua
@@ -2,18 +2,19 @@ dofile(minetest.get_modpath("blood") .. "/api.lua")
 
 
 minetest.register_on_player_hpchange(function(player, hp_change, reason)
+
 	if not minetest.is_player(player) then
 		return hp_change
-	end
-
-	if reason.type == "drown" or hp_change <= 0 then
+	elseif reason.type == "drown"then
+		return hp_change
+	elseif reason.from == "mod" and hp_change >= 0 then
+		return hp_change
+	elseif reason.from == "mod" and reason.hunger == true and hp_change >=0  then
+		return hp_change
+	elseif hp_change > -2 then
 		return hp_change
 	end
 
-	if reason.from == "mod" and reason.hunger == true and hp_change <=0  then
-		return hp_change
-	end
-
-    blood.do_blood_effects(player, hp_change)
+	blood.do_blood_effects(player, hp_change)
 	return hp_change
 end, true)
